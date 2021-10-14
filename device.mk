ConquerOS-Devices/device_xiaomi_olives

MOD: olives: Add MiuiCamera
Signed-off-by: Chuck439 <firmankediri123@gmail.com>

device.mk
@@ -69,3 +69,12 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # Inherit from vendor blobs
 $(call inherit-product, vendor/xiaomi/olives/olives-vendor.mk)
+
+# MOD: MiuiCamera
+ifneq ($(wildcard vendor/miuicamera),)
+$(call inherit-product, vendor/miuicamera/config.mk)
+PRODUCT_COPY_FILES += \
+    vendor/miuicamera/common/proprietary/etc/device_features/olive.xml:$(TARGET_COPY_OUT_VENDOR)/etc/device_features/olive.xml \
+    vendor/miuicamera/common/proprietary/etc/device_features/olivelite.xml:$(TARGET_COPY_OUT_VENDOR)/etc/device_features/olivelite.xml \
+    vendor/miuicamera/common/proprietary/etc/device_features/olivewood.xml:$(TARGET_COPY_OUT_VENDOR)/etc/device_features/olivewood.xml
+endif

EXOdev-698/device_xiaomi_olives_S

device.mk
@@ -53,3 +53,10 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # Inherit from vendor blobs
 $(call inherit-product, vendor/xiaomi/olives/olives-vendor.mk)
+
+# MOD: MiuiCamera
+ifneq ($(wildcard vendor/miuicamera),)
+$(call inherit-product, vendor/miuicamera/config.mk)
+PRODUCT_COPY_FILES += \
+    vendor/miuicamera/common/proprietary/etc/device_features/olive.xml:$(TARGET_COPY_OUT_VENDOR)/etc/device_features/olives.xml
+endif