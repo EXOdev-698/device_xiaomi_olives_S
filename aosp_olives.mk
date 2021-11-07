95166148b96b0cdfef6538419837f2062676a871
EXOdev-698 <71149028+EXOdev-698@users.noreply.github.com>
2021-11-08 00:19:50 +0300

    Update aosp_olives.mk

--- aosp_olives.mk
+++ aosp_olives.mk
@@ -10,7 +10,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
 # Inherit some common Aosp Extended stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common.mk)
 
 # Inherit from olives device
 $(call inherit-product, device/xiaomi/olives/device.mk)
